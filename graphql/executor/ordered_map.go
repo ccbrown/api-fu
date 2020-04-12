c364fa4b443db108372f34c1462c25881ffba782
Christopher Brown <ccbrown112@gmail.com>
2020-04-11 22:04:32 -0400

    collectFields optimization

--- graphql/executor/ordered_map.go
+++ graphql/executor/ordered_map.go
@@ -6,54 +6,54 @@ import (
 	jsoniter "github.com/json-iterator/go"
 )
 
+// OrderedMapItem is a key-value pair for an item in an OrderedMap.
 type OrderedMapItem struct {
 	Key   string
 	Value interface{}
 }
 
+// OrderedMap represents a map that maintains the order of its key-value pairs. It's more or less
+// just a list that serializes to a JSON map.
 type OrderedMap struct {
-	m     map[string]int
 	items []OrderedMapItem
 }
 
+// NewOrderedMap creates a new ordered map.
 func NewOrderedMap() *OrderedMap {
+	return &OrderedMap{}
+}
+
+// NewOrderedMapWithCapacity creates a new ordered map with n elements pre-allocated and
+// zero-initialized.
+func NewOrderedMapWithLength(n int) *OrderedMap {
 	return &OrderedMap{
-		m: map[string]int{},
+		items: make([]OrderedMapItem, n),
 	}
 }
 
-func NewOrderedMapWithCapacity(n int) *OrderedMap {
-	return &OrderedMap{
-		m:     make(map[string]int, n),
-		items: make([]OrderedMapItem, 0, n),
+// Set writes a key-value pair to the map at the given index.
+func (m *OrderedMap) Set(index int, key string, value interface{}) {
+	m.items[index] = OrderedMapItem{
+		Key:   key,
+		Value: value,
 	}
 }
 
-// Sets the value for a given key. The order of the key-value pairs is based on the first time a key
-// is set for the map. Overwriting an existing value does not change the order.
-func (m *OrderedMap) Set(key string, value interface{}) {
-	if idx, ok := m.m[key]; !ok {
-		m.m[key] = len(m.items)
-		m.items = append(m.items, OrderedMapItem{
-			Key:   key,
-			Value: value,
-		})
-	} else {
-		m.items[idx].Value = value
-	}
+// Append appends a key-value pair to the map. It is the caller's responsibility to make sure the
+// key doesn't already exist in the map.
+func (m *OrderedMap) Append(key string, value interface{}) {
+	m.items = append(m.items, OrderedMapItem{
+		Key:   key,
+		Value: value,
+	})
 }
 
-func (m *OrderedMap) Get(key string) (interface{}, bool) {
-	if idx, ok := m.m[key]; ok {
-		return m.items[idx].Value, true
-	}
-	return nil, false
-}
-
+// Len returns the length of the map.
 func (m *OrderedMap) Len() int {
-	return len(m.m)
+	return len(m.items)
 }
 
+// Items provides the items in the map, in the order they were added.
 func (m *OrderedMap) Items() []OrderedMapItem {
 	return m.items
 }
@@ -68,6 +68,7 @@ func (e *orderedMapEncoder) IsEmpty(ptr unsafe.Pointer) bool {
 	m := *((*OrderedMap)(ptr))
 	return m.Len() == 0
 }
+
 func (e *orderedMapEncoder) Encode(ptr unsafe.Pointer, stream *jsoniter.Stream) {
 	m := *((*OrderedMap)(ptr))
 	stream.WriteObjectStart()
