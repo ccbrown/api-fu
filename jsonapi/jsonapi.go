e6c604cbf738b12840a8a3527d032051a7836d54
ccbrown <chris@tempus-ex.com>
2023-08-20 17:26:45 -0400

    readme notes

--- jsonapi/jsonapi.go
+++ jsonapi/jsonapi.go
@@ -1,3 +1,5 @@
+// This is a package for creating [JSON:API](https://jsonapi.org) APIs. It is currently still
+// experimental and not subject to any compatibility guarantees.
 package jsonapi
 
 import (
